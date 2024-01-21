95a609535747e9182610b9adc7e8bbb7f18ae7bb
Merami6f <feelmysoul49@gmail.com>
2024-01-21 08:01:19 +0700

    add some basic pkg

--- basic_pkg.sh
+++ basic_pkg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a demo=(fzf fd zsh curl wget ripgrep bat lsd xclip unzip)
+declare -a demo=(fzf fd zsh curl wget ripgrep bat lsd xclip unzip cmake bear ninja)
 
   for z in ${demo[@]} 
   do
