f07c3bdd583b17685e8bc6b55a1291f68752acba
Merami6f <feelmysoul49@gmail.com>
2024-02-11 09:27:41 +0700

    add p10k as theme

--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir ~/.config/zsh
-mkdir ~/.config/zsh/plugins
+mkdir ~/.config/zsh-plugins
 
-git clone https://github.com/zsh-users/zsh-history-substring-search ~/.config/zsh/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh-plugins/powerlevel10k
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.config/zsh-plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh-plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh-plugins/zsh-syntax-highlighting
