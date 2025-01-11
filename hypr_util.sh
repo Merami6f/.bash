8a829619973482392a298e2ce932d7884c283b01
Merami6f <feelmysoul49@gmail.com>
2025-01-11 10:56:09 +0700

    20250111 add pavicontrol-qt

--- hypr_util.sh
+++ hypr_util.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a util=(rofi-wayland waybar hypridle hyprlock cliphist wl-clipboard hyprpaper)
+declare -a util=(rofi-wayland waybar hypridle hyprlock cliphist wl-clipboard hyprpaper pavucontrol-qt libnotify)
 
 for u in ${util[@]}
 do
