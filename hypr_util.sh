02283be80be0fe44b1a764631a5b7791c728f194
Merami6f <feelmysoul49@gmail.com>
2025-01-16 10:38:41 +0700

    250116 polkit kde

--- hypr_util.sh
+++ hypr_util.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a util=(rofi-wayland waybar hypridle hyprlock cliphist wl-clipboard hyprpaper pavucontrol-qt libnotify)
+declare -a util=(isoimagewriter polkit-kde-agent rofi-wayland waybar hypridle hyprlock cliphist wl-clipboard hyprpaper pavucontrol-qt libnotify)
 
 for u in ${util[@]}
 do
