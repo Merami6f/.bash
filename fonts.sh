d0747a23a48182ff3a4f959c32e2aa64eebc92f1
Merami6f <feelmysoul49@gmail.com>
2026-06-07 10:55:24 +0700

    260607

--- fonts.sh
+++ fonts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a fonts=(ttf-nerd-fonts-symbols noto-fonts-cjk)
+declare -a fonts=(ttf-nerd-fonts-symbols noto-fonts-cjk noto-fonts-emoji ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono)
 
 for u in ${fonts[@]}
 do
